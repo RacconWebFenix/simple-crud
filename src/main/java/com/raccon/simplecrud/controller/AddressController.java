c063c6941a5ab367236833f75932536cd2022e2f
Marcos César Domingues <dfenixweb@gmail.com>
2024-04-04 18:10:13 -0300

    refact addrdresses annotations

--- src/main/java/com/raccon/simplecrud/controller/AddressController.java
+++ src/main/java/com/raccon/simplecrud/controller/AddressController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.raccon.simplecrud.dto.AddressDTO;
 import com.raccon.simplecrud.model.addresses.Address;
 import com.raccon.simplecrud.services.AddressService;
-import com.raccon.simplecrud.utils.SwaggerAnotations.CreateAddressRequestBody;
+import com.raccon.simplecrud.utils.SwaggerAnotations.AddressAnnotations;
+import com.raccon.simplecrud.utils.SwaggerAnotations.AddressAnnotations.CreateAddressRequestBody;
+import com.raccon.simplecrud.utils.SwaggerAnotations.AddressAnnotations.GetAllAddressRequestBody;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -26,32 +28,21 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-
 @RestController
 @RequestMapping("/api/address")
-@Tag(name = "Address", description = "Endpoints para operações de endereços")
+@Tag(name = "Endereços", description = "Endpoints para operações de endereços")
 public class AddressController {
 
     @Autowired
     private AddressService addressService;
 
-    @Operation(summary = "Lista Endereços")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Lista de endereços retornada com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AddressDTO.class))),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content(mediaType = "application/json"))
-    })
+    @GetAllAddressRequestBody
     @GetMapping
     public ResponseEntity<List<Address>> getMethodName() {
         List<Address> addresses = addressService.getAllAddress();
         return ResponseEntity.status(HttpStatus.OK).body(addresses);
     }
 
-    @Operation(summary = "Criar Endereço")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Endereço criado com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AddressDTO.class))),
-            @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content(mediaType = "application/json"))
-    })
     @CreateAddressRequestBody
     @PostMapping
     public ResponseEntity<Address> createAddress(@RequestBody Address addressDTO) {
---
+++ src/main/java/com/raccon/simplecrud/utils/SwaggerAnotations/AddressAnnotations.java
@@ -0,0 +1,49 @@
+package com.raccon.simplecrud.utils.SwaggerAnotations;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.raccon.simplecrud.dto.AddressDTO;
+
+public class AddressAnnotations {
+
+        @Retention(RetentionPolicy.RUNTIME)
+        @Target(ElementType.METHOD)
+        @Operation(summary = "Lista Endereços")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Lista de endereços retornada com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AddressDTO.class))),
+                        @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content(mediaType = "application/json"))
+        })
+        public @interface GetAllAddressRequestBody {
+        }
+
+        @Retention(RetentionPolicy.RUNTIME)
+        @Target(ElementType.METHOD)
+        @Operation(summary = "Cria Endereço")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "201", description = "Endereço criado com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AddressDTO.class))),
+                        @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content(mediaType = "application/json")),
+                        @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content(mediaType = "application/json"))
+        })
+        @RequestBody(description = "Dados do endereço a ser criado", required = true, content = @Content(mediaType = "application/json", schema = @Schema(implementation = AddressDTO.class), examples = @ExampleObject(value = "{\n"
+                        +
+                        "  \"personId\": 1,\n" +
+                        "  \"street\": \"Rua ABC\",\n" +
+                        "  \"city\": \"Cidade XYZ\",\n" +
+                        "  \"state\": \"Estado XYZ\",\n" +
+                        "  \"postalCode\": \"12345-678\"\n" +
+                        "}")))
+        public @interface CreateAddressRequestBody {
+        }
+
+}
