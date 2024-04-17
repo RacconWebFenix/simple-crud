d9ceb069aae244447b3a63ee1325441b800bb2e2
Marcos César Domingues <dfenixweb@gmail.com>
2024-04-17 08:38:36 -0300

    disable cors

--- src/main/java/com/raccon/simplecrud/controller/AuthenticationController.java
+++ src/main/java/com/raccon/simplecrud/controller/AuthenticationController.java
@@ -25,6 +25,8 @@ import com.raccon.simplecrud.services.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
